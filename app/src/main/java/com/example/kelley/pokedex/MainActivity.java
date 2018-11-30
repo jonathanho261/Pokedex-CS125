56c84a2a5ac6bc33eb148fd83189a4474a11e867
jho <jaho2@illinois.edu>
2018-11-30 11:17:16 -0600

    added volley

--- app/src/main/java/com/example/kelley/pokedex/MainActivity.java
+++ app/src/main/java/com/example/kelley/pokedex/MainActivity.java
@@ -2,12 +2,55 @@ package com.example.kelley.pokedex;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+
+import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
+
+    /** Default logging tag for messages from the main activity. */
+    private static final String TAG = "Pokedex-TAG";
+
+    /** Request queue for our network requests. */
+    private static RequestQueue requestQueue;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    }
+
+    void startAPICall(final String str) {
+        try {
+            JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(
+                    Request.Method.GET,
+                    "https://pokeapi.co/api/v2/pokemon/" + str,
+                    null,
+                    new Response.Listener<JSONObject>() {
+                        @Override
+                        public void onResponse(final JSONObject response) {
+                            Log.d(TAG, response.toString());
+                        }
+                    }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(final VolleyError error) {
+                    Log.w(TAG, error.toString());
+                }
+            });
+            requestQueue.add(jsonObjectRequest);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
     }
 }
